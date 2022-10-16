f40d5e9ed002743006e4f227938fca60a863f332
‘Kanata <bearpooh.kanayan@gmail.com>
2022-10-16 17:59:53 +0900

    2022 速度改善 JS設定依頼・コピーライト

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
                 fullDate
             window.location.href = link;
         }
+        setTimeout("load()", 0);
     </script>
     <style>
         * {
@@ -31,12 +32,26 @@
         h1 {
             color: #ff0000;
         }
+
+        p{
+            font-size: 15px;
+        }
+
+        a{
+            font-size: 15px;
+            color: #00f;
+        }
+
     </style>
 </head>
 
-<body onload="load();">
+<body>
     <b><h1>ページ転送中</h1></b>
     <h2>この画面のまま<br>お待ちください</h2>
+    <p>画面が変わらない場合は<a href="https://docs.google.com/forms/d/e/1FAIpQLSc16HQ7iztjqe_ryHezHBQ_gTBy0r-WV2J7hlcwDEy2oJl9Yg/viewform">こちらをタップ</a></p>
+
+    <p>画面が変わらない場合は<br>設定→Safari→詳細→JavaScriptをオンにすると<br>正常に動作する場合があります</p>
+    <p>&copy; 2022 <a href="link.pooh.fun">Kanata Tsuda</a> All Right Reserved.</p>
 </body>
 
 </html>
