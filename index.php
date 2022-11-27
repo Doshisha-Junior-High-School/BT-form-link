e97a1d31defd2f8689e4510f5138e81272b790de
Kanata Tsuda <94170718+bearpoohkanayan@users.noreply.github.com>
2022-11-27 23:14:53 +0900

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <body>
     <h1>お知らせ</h1>
     <h2>使用していたサービスの有料化に伴い、URLを変更しました。</h2>
-    <h2>本日15時よりこのURLでは使用できなくなります。<br>ブックマークなどの更新をお願いします。</h2>
+    <h2>11月28日15時よりこのURLでは使用できなくなります。<br>ブックマークなどの更新をお願いします。</h2>
     <h2>新リンクはこちら↓</h2>
     <h3><a href="https://djhs-bt-form.netlify.app/">https://djhs-bt-form.netlify.app/</a></h3>
     <p>&copy; 2022 <a href="//link.pooh.fun">Kanata Tsuda(d021332)</a> All Right Reserved.</p>
